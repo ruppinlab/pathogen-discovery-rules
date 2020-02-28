4130442ebf94f57ca963b3ae48c0dd90f46508ae
Wells Ivens Robinson <wir963@gmail.com>
2020-02-28 10:47:10 -0500

    use multi-threading to take adv of the cluster

--- src/filter_fna_files.py
+++ src/filter_fna_files.py
@@ -1,6 +1,22 @@
 from Bio import SeqIO
+from multiprocessing import Pool
+import itertools
 
-for fna_file in snakemake.input:
-    for record in SeqIO.parse(fna_file, "fasta"):
-        if record.description.contains("Salmonella enterica"):
-            print(record.description)
+
+bacteria_of_interest = ["Salmonella enterica subsp. enterica serovar Typhimurium str. SL1344", "Salmonella enterica subsp. enterica serovar Typhimurium str. LT2"]
+
+
+def identify_specific_sequences(f):
+    output = []
+    for record in SeqIO.parse(f, "fasta"):
+        if any(b in record.description for b in bacteria_of_interest):
+            output.append(record)
+    return output
+
+
+if __name__ == '__main__':
+    with Pool(31) as p:
+        list_of_lists = p.map(identify_specific_sequences, snakemake.input)
+        flat_list = list(itertools.chain(*list_of_lists)) 
+        for r in flat_list:
+            print(r.description)
